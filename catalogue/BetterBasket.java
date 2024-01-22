cdef6a66bb096ce8f97bd24d8e6980b2993e758a
libbi <liberty.pr11@gmail.com>
2024-01-22 16:14:41 +0000

    tt

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -12,7 +12,26 @@ import java.util.Collections;
 public class BetterBasket extends Basket implements Serializable
 {
   private static final long serialVersionUID = 1L;
+  
+  public boolean add(Product pr)
+  {
+	  for(int index=0; index<size();index++)
+	  {
+		  if (get(index).getProductNum().equals(pr.getProductNum()))
+		  {
+			  get(index).setQuantity(get(index).getQuantity()+pr.getQuantity());
+			  return true;
+		  }
+	  }
+	  super.add(pr);
+	  Collections.sort(this,BetterBasket::arrange);
+	  return true;
+  }
  
+  public static int arrange(Product a, Product b)
+  {
+	  return a.getProductNum().compareTo(b.getProductNum());
+  }
 
 
   // You need to add code here
--- catalogue/BetterBasketTest.java
+++ catalogue/BetterBasketTest.java
@@ -7,8 +7,23 @@ import org.junit.jupiter.api.Test;
 class BetterBasketTest {
 
 	@Test
-	void testAddProduct() {
-		fail("Not yet implemented");
+	void testMergeAddProduct() {
+		BetterBasket b = BetterBasket();
+		Product p1= new Product("0001", "Toaster", 10.00, 1);
+		Product p2= new Product("0001", "Toaster", 10.00, 1);
+		Product p3= new Product("0002", "Kettle", 15.00, 1);
+		Product p4 = new Product("0002", "Kettle", 15.00, 2);
+		
+		b.add(p1);
+		b.add(p2);
+		assertEquals(1, b.size(), "Size incorrect after merge");
+		assertEquals(2, b.get(0).getQuantity(), "Quantity incorrect after merge");
+		
+		b.add(p3);
+		assertEquals(2, b.size(), "Size incorrect after non-merge");
+		
+		b.add(p4);
+		assertEquals(3, b.size(), "Size incorrect after non-merge");
 	}
 
 }
